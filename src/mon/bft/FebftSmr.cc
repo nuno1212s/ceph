7a1e79a4a41c6148dae1af730e71362c0f2d489d
Nuno Neto <nuno.neto.g@gmail.com>
2022-09-19 19:38:40 +0100

    Do not refresh on start.

--- src/mon/bft/FebftSmr.cc
+++ src/mon/bft/FebftSmr.cc
@@ -91,8 +91,6 @@ void FebftSMR::init() {
             dout(10) << __func__ << " running febft replica " << dendl;
             start_replica_thread(this->replica);
             dout(10) << __func__ << " Started running the replica, refreshing" << dendl;
-
-            this->handle_committed_values(0);
         }
 //    });
     }
